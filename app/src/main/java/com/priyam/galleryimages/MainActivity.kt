3cc953a509c16b87da63a159b67a3a038abc72cd
Priyam Prakash <65361533+priyamprakash@users.noreply.github.com>
2022-12-29 16:38:58 +0530

    Design

--- app/src/main/java/com/priyam/galleryimages/MainActivity.kt
+++ app/src/main/java/com/priyam/galleryimages/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.priyam.galleryimages
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageSwitcher
 
 class MainActivity : AppCompatActivity() {
+    private var images: ArrayList<Uri?>? = null
+    private lateinit var next: Button
+    private lateinit var previous: Button
+    private lateinit var pick: Button
+    private lateinit var imageSwitcher: ImageSwitcher
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        next = findViewById(R.id.nextButton)
+        previous = findViewById(R.id.previousButton)
+        pick = findViewById(R.id.pickImage)
+        imageSwitcher = findViewById(R.id.imageSwitcher)
+
+        images = ArrayList()
+
+
     }
 }
