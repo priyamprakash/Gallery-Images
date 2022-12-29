9aa46c8732d02c96b6a4c22074712ede84582b35
Priyam Prakash <65361533+priyamprakash@users.noreply.github.com>
2022-12-29 16:52:34 +0530

    Update MainActivity.kt

--- app/src/main/java/com/priyam/galleryimages/MainActivity.kt
+++ app/src/main/java/com/priyam/galleryimages/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.priyam.galleryimages
 
+import android.app.Activity
+import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,6 +10,9 @@ import android.widget.ImageSwitcher
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+    private var position = 0
+    private val PICK_IMAGES_CODE = 0
+
     private var images: ArrayList<Uri?>? = null
     private lateinit var next: Button
     private lateinit var previous: Button
@@ -37,4 +42,29 @@ class MainActivity : AppCompatActivity() {
 
         }
     }
+
+    private fun pickImagesIntent(){
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE,true)
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(Intent.createChooser(intent, "Select image(s)"),PICK_IMAGES_CODE)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == PICK_IMAGES_CODE){
+          if(resultCode == Activity.RESULT_OK){
+              if(data!!.clipData != null){
+                  //picked multiple images
+              }
+              else{
+                  //picked single images
+              }
+          }
+        }
+    }
+
 }
